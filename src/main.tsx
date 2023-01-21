f79ffb16106d37bdcbfc9902cd1bd2b489614f10
tahashieenavaz <tahashieenavaz@gmail.com>
2023-01-21 22:46:53 +0100

    Removes extra provider in main file

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { AppContext } from "./contexts/AppContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <AppContext.Provider value={{ name: "Taha" }}>
-      <App />
-    </AppContext.Provider>
+    <App />
   </React.StrictMode>
 );
