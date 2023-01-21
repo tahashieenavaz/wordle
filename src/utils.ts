c3db4e0763c75a8a09444bbd71c709ec7d199299
tahashieenavaz <tahashieenavaz@gmail.com>
2023-01-21 21:04:24 +0100

    Type fixed

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 export const upperCaseAlphabet = Array(26)
   .fill()
-  .map((_, i) => String.fromCharCode(97 - 32 + i));
+  .map((_: any, i: number) => String.fromCharCode(97 - 32 + i));
 
 export const lowerCaseAlphabet = upperCaseAlphabet.map((letter: string) =>
   letter.toLowerCase()
