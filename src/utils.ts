2b36540b11e26a9df8832f240e7bf364373137f4
tahashieenavaz <tahashieenavaz@gmail.com>
2023-01-22 12:08:43 +0100

    Adds types to utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,24 +1,29 @@
 import { words } from './data/words'
 
-export const upperCaseAlphabet = Array(26)
+export const upperCaseAlphabet: Array<string> = Array(26)
   .fill()
   .map((_: any, i: number) => String.fromCharCode(97 - 32 + i))
 
-export const lowerCaseAlphabet = upperCaseAlphabet.map((letter: string) =>
-  letter.toLowerCase()
+export const lowerCaseAlphabet: Array<string> = upperCaseAlphabet.map(
+  (letter: string) => letter.toLowerCase()
 )
 
-export const alphabet = [...lowerCaseAlphabet, ...upperCaseAlphabet]
+export const alphabet: Array<string> = [
+  ...lowerCaseAlphabet,
+  ...upperCaseAlphabet,
+]
 
-export const keyboard = [['QWERTYUIOP'], ['ASDFGHJKL'], ['ZXCVBNM']].map(
-  (row: Array<string>) => row[0].split('')
-)
+export const keyboard: Array<Array<string>> = [
+  ['QWERTYUIOP'],
+  ['ASDFGHJKL'],
+  ['ZXCVBNM'],
+].map((row: Array<string>) => row[0].split(''))
 
 export const wordListContains = (word: string): boolean => {
   word = word.toLowerCase()
   return Boolean(words[word])
 }
 
-export const isFiveCharLong = (word: string) => {
+export const isFiveCharLong = (word: string): boolean => {
   return word.length === 5
 }
